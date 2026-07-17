a5ffbb9c96048d77958bc7a0844b4b937a0dd70c
mexanik-it <dda777@yandex.ru>
2026-07-17 14:10:31 +0300

    update README.md

--- README.md
+++
@@ -1,9 +0,0 @@
-# mex-lib
-своя библиотека для работы с g++
-
-#### Скачать локально
-```
-git clone https://github.com/mexanik-it/mex-lib.git
-```
-
-### 
--- windows/source/example_color.cpp
+++ windows/source/example_color.cpp
@@ -1,9 +1,12 @@
-#include <mex-lib.h>
-#include <iostream>
 
-int example_color ( ) {
-    std::cout << Colors::brown  << "===|" << Colors::green  << "  Created by Dmitry Denkin (c) 2026  " << Colors::brown << "|======" << std::endl;
-    std::cout << Colors::blue    << " *******          " << Colors::cyan << " S y s t e m   I n f o r m a t i o n " << Colors::blue   << "******" << Colors::reset << std::endl;
-    
+#include <iostream>
+#include "colors.h"
+
+int example_color() {
+    std::cout << Colors::brown  << "===|" 
+              << Colors::green  << "  Created by Dmitry Denkin (c) 2026  " 
+              << Colors::brown  << "|===" 
+              << Colors::reset 
+              << std::endl;
     return 0;
 }
